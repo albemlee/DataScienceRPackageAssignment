c618a5befcbfbae276ddb22c0d298c35658d8acc
albemlee <albemlee@umich.edu>
2018-02-19 14:17:22 -0500

    first commit

---
+++ R/fars_functions.R
@@ -0,0 +1,131 @@
+#' Read fars data file
+#'
+#' This is a function that returns a tibble based on a csv.bz2 file.
+#' 
+#' @importFrom dplyr
+#'
+#' @param filename A character string or integer representing the location of
+#'    the file to be read.
+#' 
+#' @return This function returns a tibble that includes the data in filename.
+#'
+#' @export
+fars_read <- function(filename) {
+        if(!file.exists(filename))
+                stop("file '", filename, "' does not exist")
+        data <- suppressMessages({
+                readr::read_csv(filename, progress = FALSE)
+        })
+        dplyr::tbl_df(data)
+}
+
+#' Create file
+#'
+#' This is a function that creates a filename based on the year.
+#'
+#' @param year A character string representing the year.
+#' 
+#' @return This function returns the filename that corresponds to year.
+#'
+#' @export
+make_filename <- function(year) {
+        year <- as.integer(year)
+        sprintf("accident_%d.csv.bz2", year)
+}
+
+#' Read fars data files based on a list of years.
+#'
+#' This is a function that reads multiple fars data files based on a list of 
+#' years.
+#'
+#' @importFrom dplyr
+#'
+#' @param years A list of integers representing the years to include.
+#' 
+#' @return This function returns a list with tibbles for every year included.
+#'
+#' @examples
+#' fars_read_years(2013)
+#' fars_read_years(2013:2015)
+#' fars_read_years(c(2013, 2015))
+#'
+#' @export
+fars_read_years <- function(years) {
+        lapply(years, function(year) {
+                file <- make_filename(year)
+                tryCatch({
+                        dat <- fars_read(file)
+                        dplyr::mutate(dat, year = year) %>% 
+                                dplyr::select(MONTH, year)
+                }, error = function(e) {
+                        warning("invalid year: ", year)
+                        return(NULL)
+                })
+        })
+}
+
+#' Summarize the data based on years
+#'
+#' This is a function that reads multiple fars data files based on a list of 
+#' years and provides the sum of data points for each year and month
+#'
+#' @importFrom dplyr
+#' @importFrom tidyr
+#'
+#' @param years A list of integers representing the years to include.
+#' 
+#' @return This function returns a tibble with the number of accidents grouped
+#'    by year and month.
+#'
+#' @examples
+#' fars_summarize_years(2013)
+#' fars_summarize_years(2013:2015)
+#' fars_summarize_years(c(2013, 2015))
+#'
+#' @export
+fars_summarize_years <- function(years) {
+        dat_list <- fars_read_years(years)
+        dplyr::bind_rows(dat_list) %>% 
+                dplyr::group_by(year, MONTH) %>% 
+                dplyr::summarize(n = n()) %>%
+                tidyr::spread(year, n)
+}
+
+#' Print a map showing where accidents occured
+#'
+#' This is a function that prints a map of the locations of accidents in a
+#'    particular state and year.
+#'
+#' @importFrom dplyr
+#' @importFrom map
+#'
+#' @param state.num An integer representing the state.
+#'
+#' @param year An integer representing the year.
+#' 
+#' @return This function returns a map of locations of accidents.
+#'
+#' @examples
+#' fars_map_state(1, 2013)
+#'
+#' @export
+fars_map_state <- function(state.num, year) {
+        filename <- make_filename(year)
+        data <- fars_read(filename)
+        state.num <- as.integer(state.num)
+
+        if(!(state.num %in% unique(data$STATE)))
+                stop("invalid STATE number: ", state.num)
+        data.sub <- dplyr::filter(data, STATE == state.num)
+        if(nrow(data.sub) == 0L) {
+                message("no accidents to plot")
+                return(invisible(NULL))
+        }
+        is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
+        is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
+        with(data.sub, {
+                maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
+                          xlim = range(LONGITUD, na.rm = TRUE))
+                graphics::points(LONGITUD, LATITUDE, pch = 46)
+        })
+}
